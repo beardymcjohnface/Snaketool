947e3854e270a0d7814fd6ec7dc3b7162b33b17a
Michael Roach <beardymcjohnface@gmail.com>
2022-08-04 15:25:02 +0930

    fix

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
@@ -125,8 +125,8 @@ def run_snakemake(configfile=None, snakefile_path=None, merge_config=None, profi
         snake_command += snake_extra
 
     # Run Snakemake!!!
-    snake_command = list(map(str,snake_command))
-    msg_box('Snakemake command', errmsg=' '.join(snake_command))
+    snake_command = ' '.join(str(s) for s in snake_command)
+    msg_box('Snakemake command', errmsg=snake_command)
     if not subprocess.run(snake_command, shell=True).returncode == 0:
         msg('Error: Snakemake failed')
         sys.exit(1)
