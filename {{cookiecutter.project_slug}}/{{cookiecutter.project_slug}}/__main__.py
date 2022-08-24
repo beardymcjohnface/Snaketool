9378a5ec619da8b4095581a0c72bc1178e80c2c0
Michael Roach <beardymcjohnface@gmail.com>
2022-08-24 10:16:31 +0930

    simplify the launcher for customisation

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
@@ -2,137 +2,29 @@
 Entrypoint for {{cookiecutter.project_name}}
 """
 
-import sys
 import os
-import subprocess
-import yaml
-from shutil import copyfile
-from time import localtime, strftime
-
 import click
 
-
-"""MISC FUNCTIONS
-You shouldn't need to tweak these much if at all
-- Set a different default system config file in copy_config() if you need to"""
+from .util import snake_base, print_version, copy_config, run_snakemake, OrderedCommands
 
 
-def snake_base(rel_path):
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), rel_path)
+"""CUSTOMISE YOUR LAUNCHER!
+Add any extra common command line options to common_options(). 
+These are mostly just boilerplate for running Snakemake.
 
+MAIN SCRIPT run()
+Add or edit any command line args related to running the main script here, customise the epilog etc.
+Currently, the only required input is --input which is simply passed as config to Snakemake.
 
-def print_version():
-    with open(snake_base('{{cookiecutter.project_slug}}.VERSION'), 'r') as f:
-        version = f.readline()
-    click.echo('\n' + '{{cookiecutter.project_name}} version ' + version + '\n')
-
-
-def msg(err_message):
-    tstamp = strftime('[%Y:%m:%d %H:%M:%S] ', localtime())
-    click.echo(tstamp + err_message)
-
-
-def msg_box(splash, errmsg=None):
-    msg('-' * (len(splash) + 4))
-    msg(f'| {splash} |')
-    msg(('-' * (len(splash) + 4)))
-    if errmsg:
-        click.echo('\n' + errmsg)
-
-
-def copy_config(local_config, system_config=snake_base(os.path.join('config', 'config.yaml'))):
-    if not os.path.isfile(local_config):
-        msg(f'Copying system default config to {local_config}')
-        copyfile(system_config, local_config)
-    else:
-        msg(f'Config file {local_config} already exists. Using existing config file.')
-
-
-def read_config(file):
-    with open(file, 'r') as stream:
-        _config = yaml.safe_load(stream)
-    return _config
-
-
-def write_config(_config, file):
-    msg(f'Writing runtime config file to {file}')
-    with open(file, 'w') as stream:
-        yaml.dump(_config, stream)
-
-
-class OrderedCommands(click.Group):
-    """Preserve the order of subcommands when printing --help"""
-    def list_commands(self, ctx: click.Context):
-        return list(self.commands)
-
-
-"""RUN A SNAKEFILE
-Hopefully you shouldn't need to tweak this function at all.
-- You must provide a Snakefile, all else is optional
-- Highly recommend supplying a configfile and the default snakemake args"""
-
-
-def run_snakemake(configfile=None, snakefile_path=None, merge_config=None, threads=1, use_conda=False,
-                  conda_frontend=None, conda_prefix=None, snake_default_args=None, snake_extra=None):
-    """Run a Snakefile"""
-    snake_command = ['snakemake', '-s', snakefile_path]
-
-    # if using a configfile
-    if configfile:
-        # copy sys default config if needed
-        copy_config(configfile)
-
-        # read the config
-        snake_config = read_config(configfile)
-
-        # merge in command line config if provided
-        if merge_config:
-            snake_config.update(merge_config)
-
-        # update config file for Snakemake execution
-        write_config(snake_config, configfile)
-        snake_command += ['--configfile', configfile]
-
-        # display the runtime configuration
-        msg_box('Runtime config', errmsg=yaml.dump(snake_config, Dumper=yaml.Dumper))
-
-    # add threads
-    snake_command += ['--cores', threads]
-
-    # add conda args if using conda
-    if use_conda:
-        snake_command += ['--use-conda']
-        if conda_frontend:
-            snake_command += ['--conda-frontend', conda_frontend]
-        if conda_prefix:
-            snake_command += ['--conda-prefix', conda_prefix]
-
-    # add snakemake default args
-    if snake_default_args:
-        snake_command += snake_default_args
-
-    # add any additional snakemake commands
-    if snake_extra:
-        snake_command += list(snake_extra)
-
-    # Run Snakemake!!!
-    snake_command = ' '.join(str(s) for s in snake_command)
-    msg_box('Snakemake command', errmsg=snake_command)
-    if not subprocess.run(snake_command, shell=True).returncode == 0:
-        msg('Error: Snakemake failed')
-        sys.exit(1)
-    else:
-        msg('Snakemake finished successfully')
-    return 0
-
-
-"""COMMON OPTIONS
-Any click options that will be used in more than one subcommand can be defined here.
+SUBCOMMAND EXAMPLE config()
+Copy the system default config file to working directory.
 """
 
 
 def common_options(func):
-    """Common options decorator for use with click commands."""
+    """Common command line args
+    Define common command line args here, and include them with the @common_options decorator below.
+    """
     options = [
         click.option('--output', help='Output directory', type=click.Path(),
                      default='{{cookiecutter.project_slug}}.out', show_default=True),
@@ -155,11 +47,6 @@ def common_options(func):
     return func
 
 
-"""COMMAND LINE ARGS
-Customise your launcher!
-"""
-
-
 @click.group(cls=OrderedCommands)
 def cli():
     """For more options, run:
@@ -167,13 +54,7 @@ def cli():
     pass
 
 
-"""MAIN SCRIPT run()
-Add or edit any command line args related to running the main script here, customise the epilog etc.
-The only required input is --input which is simple passed to the config.
-"""
-
-
-EPILOG = """
+help_message_extra = """
 \b
 CLUSTER EXECUTION:
 {{cookiecutter.project_slug}} run ... --profile [profile]
@@ -193,12 +74,16 @@ Available targets:
 """
 
 
-@click.command(epilog=EPILOG, context_settings={"ignore_unknown_options": True})
+@click.command(epilog=help_message_extra, context_settings={"ignore_unknown_options": True})
 @click.option('--input', '_input', help='Input file/directory', type=str, required=True)
 @common_options
 def run(_input, configfile, output, threads, use_conda, conda_frontend, conda_prefix, snake_default,
         snake_args, **kwargs):
     """Run {{cookiecutter.project_name}}"""
+
+    # copy default config file if missing
+    copy_config(configfile, system_config=snake_base(os.path.join('..', 'config', 'config.yaml')))
+
     # Config to add or update in configfile
     merge_config = {
         'input': _input,
@@ -218,16 +103,11 @@ def run(_input, configfile, output, threads, use_conda, conda_frontend, conda_pr
     )
 
 
-"""SUBCOMMAND EXAMPLE
-Copy the system default config file to working directory.
-"""
-
-
 @click.command()
 @click.option('--configfile', default='config.yaml', help='Copy template config to file', show_default=True)
 def config(configfile, **kwargs):
     """Copy the system default config file"""
-    copy_config(configfile)
+    copy_config(configfile, system_config=snake_base(os.path.join('..', 'config', 'config.yaml')))
 
 
 cli.add_command(run)
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/util.py
@@ -0,0 +1,116 @@
+"""MISC FUNCTIONS
+You shouldn't need to tweak these much if at all
+"""
+
+import sys
+import os
+import subprocess
+import yaml
+from shutil import copyfile
+from time import localtime, strftime
+
+
+class OrderedCommands(click.Group):
+    """This class will preserve the order of subcommands, which is useful when printing --help"""
+    def list_commands(self, ctx: click.Context):
+        return list(self.commands)
+
+
+def snake_base(rel_path):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), rel_path)
+
+
+def print_version():
+    with open(snake_base('{{cookiecutter.project_slug}}.VERSION'), 'r') as f:
+        version = f.readline()
+    click.echo('\n' + '{{cookiecutter.project_name}} version ' + version + '\n')
+
+
+def msg(err_message):
+    tstamp = strftime('[%Y:%m:%d %H:%M:%S] ', localtime())
+    click.echo(tstamp + err_message)
+
+
+def msg_box(splash, errmsg=None):
+    msg('-' * (len(splash) + 4))
+    msg(f'| {splash} |')
+    msg(('-' * (len(splash) + 4)))
+    if errmsg:
+        click.echo('\n' + errmsg)
+
+
+def copy_config(local_config, system_config=None):
+    if not os.path.isfile(local_config):
+        msg(f'Copying system default config to {local_config}')
+        copyfile(system_config, local_config)
+    else:
+        msg(f'Config file {local_config} already exists. Using existing config file.')
+
+
+def read_config(file):
+    with open(file, 'r') as stream:
+        _config = yaml.safe_load(stream)
+    return _config
+
+
+def write_config(_config, file):
+    msg(f'Writing runtime config file to {file}')
+    with open(file, 'w') as stream:
+        yaml.dump(_config, stream)
+
+
+"""RUN A SNAKEFILE
+Hopefully you shouldn't need to tweak this function at all.
+- You must provide a Snakefile, all else is optional
+- Highly recommend supplying a configfile and the default snakemake args"""
+
+
+def run_snakemake(configfile=None, snakefile_path=None, merge_config=None, threads=1, use_conda=False,
+                  conda_frontend=None, conda_prefix=None, snake_default_args=None, snake_extra=None):
+    """Run a Snakefile"""
+    snake_command = ['snakemake', '-s', snakefile_path]
+
+    # if using a configfile
+    if configfile:
+        # read the config
+        snake_config = read_config(configfile)
+
+        # merge in command line config if provided
+        if merge_config:
+            snake_config.update(merge_config)
+
+        # update config file for Snakemake execution
+        write_config(snake_config, configfile)
+        snake_command += ['--configfile', configfile]
+
+        # display the runtime configuration
+        msg_box('Runtime config', errmsg=yaml.dump(snake_config, Dumper=yaml.Dumper))
+
+    # add threads
+    snake_command += ['--cores', threads]
+
+    # add conda args if using conda
+    if use_conda:
+        snake_command += ['--use-conda']
+        if conda_frontend:
+            snake_command += ['--conda-frontend', conda_frontend]
+        if conda_prefix:
+            snake_command += ['--conda-prefix', conda_prefix]
+
+    # add snakemake default args
+    if snake_default_args:
+        snake_command += snake_default_args
+
+    # add any additional snakemake commands
+    if snake_extra:
+        snake_command += list(snake_extra)
+
+    # Run Snakemake!!!
+    snake_command = ' '.join(str(s) for s in snake_command)
+    msg_box('Snakemake command', errmsg=snake_command)
+    if not subprocess.run(snake_command, shell=True).returncode == 0:
+        msg('Error: Snakemake failed')
+        sys.exit(1)
+    else:
+        msg('Snakemake finished successfully')
+    return 0
